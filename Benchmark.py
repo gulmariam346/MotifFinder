ebc516b1845c273ca97c9d34a51eec9faef5dda9
arpitmathur <arpitdevmathur@gmail.com>
2015-04-23 19:03:35 -0500

    generating sequences and strings

--- Benchmark.py
+++ Benchmark.py
@@ -1,10 +1,36 @@
 __author__ = 'Arpit'
+variable = -999999
+import random
+import math
 def generate_sequences(SL,SC):
-    for i in range(1,SC):
-        print(i*SL+SC,"\n")
+    sequence_list = []
+    for i in range(0,SL):
+        random_sequence = ""
+        for i in range(0,SC):
+            random_sequence += random.choice(nucleotides)
+        sequence_list.append(random_sequence)
+    return sequence_list
 
-ML=int(input("Please enter a positive integer as the motif length\n"))
-NM=int(input("Please enter a non-negative integer as the number of variable positions\n"))
-SL=int(input("Please enter a positive integer as the sequence length\n"))
-SC=int(input("Please enter a positive integer as the sequence count\n"))
-generate_sequences(SL,SC)
+def generate_DNA_String(ML,NM):
+    DNA_String = ""
+    for i in range(0,ML):
+            DNA_String += random.choice(nucleotides)
+    global variable
+    if(ML-NM>0):
+        variable = random.randint(0, ML-NM)
+    else:
+        variable = random.randint(NM,ML)
+
+    return DNA_String
+
+ML = int(input("Please enter a positive integer as the motif length\n"))
+NM = int(input("Please enter a non-negative integer as the number of variable positions\n"))
+SL = int(input("Please enter a positive integer as the sequence length\n"))
+SC = int(input("Please enter a positive integer as the sequence count\n"))
+
+
+nucleotides = ['A','G','C','T']
+sequence_list = generate_sequences(SL,SC)
+DNA_String = generate_DNA_String(ML,NM)
+print(DNA_String)
+print(variable)
